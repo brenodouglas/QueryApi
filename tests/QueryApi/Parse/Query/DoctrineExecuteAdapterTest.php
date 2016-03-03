ca0c5b6afb1966a930e30f7e2ed071e88832cd94
Breno Douglas Araujo Souza <breno_douglas@hotmail.com>
2016-03-03 17:28:39 -0300

    Update DoctrineExecuteAdapterTest.php

--- tests/QueryApi/Parse/Query/DoctrineExecuteAdapterTest.php
+++ tests/QueryApi/Parse/Query/DoctrineExecuteAdapterTest.php
@@ -5,5 +5,9 @@ use PHPUnit_Framework_TestCase as PHPUnit;
 
 class DoctrineExecuteAdapterTest extends PHPUnit 
 {
-
+  
+  public function test_execute_query()
+  {
+      $this->assert(1, 1);
+  }
 }
